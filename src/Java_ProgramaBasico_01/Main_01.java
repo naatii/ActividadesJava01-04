98192328898688b5a9e66ee1198685a66d30c45f
naatii <jcorber855@g.educaand.es>
2024-10-05 14:13:50 +0200

    Initial commit

---
+++ src/Java_ProgramaBasico_01/Main_01.java
@@ -0,0 +1,96 @@
+package Java_ProgramaBasico_01;
+
+//Aquí van las clses de toda la relación 1
+
+class Ejercicios {
+    public static void actividad1(){
+
+    }
+
+    public static void actividad2() {
+        
+    }
+
+    public static void actividad3() {
+    }
+
+    public static void actividad4() {
+    }
+
+    public static void actividad5() {
+    }
+
+    public static void actividad6() {
+    }
+
+    public static void actividad7() {
+    }
+
+    public static void actividad8() {
+    }
+
+    public static void actividad9() {
+    }
+
+    public static void actividad10() {
+    }
+
+    public static void actividad11() {
+    }
+
+    public static void actividad12() {
+    }
+
+    public static void actividad13() {
+    }
+
+    public static void actividad14() {
+    }
+
+    public static void actividad15() {
+    }
+
+    public static void actividad16() {
+    }
+
+    public static void actividad17() {
+    }
+
+    public static void actividad18() {
+    }
+
+    public static void actividad19() {
+    }
+
+    public static void actividad20() {
+    }
+
+    public static void actividad21() {
+    }
+}
+
+public class Main_01 {
+    public static void main(String[] args) {
+        Ejercicios.actividad1();
+        Ejercicios.actividad2();
+        Ejercicios.actividad3();
+        Ejercicios.actividad4();
+        Ejercicios.actividad5();
+        Ejercicios.actividad6();
+        Ejercicios.actividad7();
+        Ejercicios.actividad8();
+        Ejercicios.actividad9();
+        Ejercicios.actividad10();
+        Ejercicios.actividad11();
+        Ejercicios.actividad12();
+        Ejercicios.actividad13();
+        Ejercicios.actividad14();
+        Ejercicios.actividad15();
+        Ejercicios.actividad16();
+        Ejercicios.actividad17();
+        Ejercicios.actividad18();
+        Ejercicios.actividad19();
+        Ejercicios.actividad20();
+        Ejercicios.actividad21();
+    }
+}
